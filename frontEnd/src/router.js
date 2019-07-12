e4443f214c36ae09941833fc4c2db6aae8f4f544
Michael Ozery <michaelozery@gmail.com>
2019-07-12 13:39:20 +0300

    ttt

--- frontEnd/src/router.js
+++ frontEnd/src/router.js
@@ -2,13 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HomePage from './views/homePage';
 import addQuiz from './views/addQuiz';
-<<<<<<< HEAD
 import logIn from './views/logIn';
 import signUp from './views/signUp';
 
-=======
 import TEST1 from './components/quizLobby.vue';
->>>>>>> 547dcc11d523145a65079efa35e60fe3348f41e7
 // import about from './views/about.vue';
 
 Vue.use(Router);
@@ -28,7 +25,6 @@ export default new Router({
       component: addQuiz
     },
     {
-<<<<<<< HEAD
       path: '/login',
       name: 'logIn',
       component: logIn
@@ -38,11 +34,9 @@ export default new Router({
       name: 'signUp',
       component: signUp
     },
-=======
       path: '/quiz/TESTING',
       name: 'TEST1',
       component: TEST1
     }
->>>>>>> 547dcc11d523145a65079efa35e60fe3348f41e7
   ]
 });
