49f585c9e09abf32964812a0660a10422e2457c4
Michael Ozery <michaelozery@gmail.com>
2019-07-12 12:30:42 +0300

    push for pull

--- frontEnd/src/views/homePage.vue
+++ frontEnd/src/views/homePage.vue
@@ -23,50 +23,6 @@
 
 <script>
 // @ is an alias to /src
-<<<<<<< HEAD
-import HelloWorld from "@/components/HelloWorld.vue";
-import appHeader from "@/components/appHeader";
-
-export default {
-  name: "home",
-  components: {
-    appHeader,
-    HelloWorld
-  }
-};
-</script>
-
-<style lang:"scss" scoped>
-section {
-  height: 80vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-}
-button {
-  padding: 20px;
-  margin: 10px 0;
-}
-
-.quizes-show {
-  border: 2px black solid;
-  padding: 10px;
-  width: 80vw;
-}
-
-.cards {
-  text-align: center;
-  display: flex;
-}
-
-.card {
-  border: 1px black solid;
-  padding: 70px 50px;
-  margin: 0 5px;
-}
-=======
 import homePage from "@/styles/views/_home-page.scss";
 
 export default {
@@ -76,5 +32,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
->>>>>>> 63a93ed4e6316e83ef815ecfbb78fa50176bd453
 </style>
