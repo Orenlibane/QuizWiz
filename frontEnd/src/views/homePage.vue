63a93ed4e6316e83ef815ecfbb78fa50176bd453
orenTz <orenlibane@gmail.com>
2019-07-12 12:27:35 +0300

    add add quiz page + connection to scss

--- frontEnd/src/views/homePage.vue
+++ frontEnd/src/views/homePage.vue
@@ -7,7 +7,9 @@
         <div v-for="(n,idx) in 8" :key="idx" class="card">quiz {{idx+1}}</div>
       </div>
     </div>
-    <button to="/quiz/edit">Add quiz</button>
+    <router-link to="/quiz/add">
+      <button>Add quiz</button>
+    </router-link>
     <div class="quizes-show">
       <h4>here is the recomendd quizes</h4>
       <div class="cards">
@@ -20,44 +22,13 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import homePage from "@/styles/views/_home-page.scss";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
 
-<style scoped>
-section {
-  height: 80vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-}
-button {
-  padding: 20px;
-  margin: 10px 0;
-}
-
-.quizes-show {
-  border: 2px black solid;
-  padding: 10px;
-  width: 80vw;
-}
-
-.cards {
-  text-align: center;
-  display: flex;
-}
-
-.card {
-  border: 1px black solid;
-  padding: 70px 50px;
-  margin: 0 5px;
-}
+<style lang="scss" scoped>
 </style>
