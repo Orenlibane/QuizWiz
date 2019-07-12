170bb5527e0b8fc471a695c040929fe75b04eeed
orenTz <orenlibane@gmail.com>
2019-07-12 11:06:57 +0300

    test push

---
+++ frontEnd/src/views/updateQuiz.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="home">
+    <nav>Nav here</nav>
+    <div class="quizes-show">
+      <h4>here is the ongoing quizes</h4>
+      <div class="cards">
+        <div v-for="(n,idx) in 8" :key="idx" class="card">quiz {{idx+1}}</div>
+      </div>
+    </div>
+    <button to="/quiz/edit">Add quiz</button>
+    <div class="quizes-show">
+      <h4>here is the recomendd quizes</h4>
+      <div class="cards">
+        <div v-for="(n,idx) in 8" :key="idx" class="card">quiz {{idx+1}}</div>
+      </div>
+    </div>
+    <footer>footer here</footer>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style scoped>
+section {
+  height: 80vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
