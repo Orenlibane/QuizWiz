60ecc4f98e4f736b1672ee0f801b8915389df191
skendis <sergei.kendis@gmail.com>
2019-07-12 12:58:43 +0300

    store test obj

--- frontEnd/src/store.js
+++ frontEnd/src/store.js
@@ -1,9 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-const quizes = [
-  
-
+let test = [
+  {
+    _id: "3d298urffgj",
+    title: "comic quiz",
+    tags: "Entertainment, Comics",
+    createdAt: 1562917080939,
+    createdBy: "3298urj",
+    bestScore: 57,
+    likesCount: 2,
+  },
+  {
+    _id: "3298urffdgj",
+    title: "comic quiz",
+    tags: "Entertainment, Comics",
+    createdAt: 1562917080939,
+    createdBy: "3298urj",
+    bestScore: 57,
+    likesCount: 2,
+  },
+  {
+    _id: "329d8urffgj",
+    title: "comic quiz",
+    tags: "Entertainment, Comics",
+    createdAt: 1562917080939,
+    createdBy: "3298urj",
+    bestScore: 57,
+    likesCount: 2,
+  }
 ]
 
 
@@ -12,12 +37,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    quizes: test
   },
   mutations: {
 
   },
   actions: {
 
+  },
+  getters: {
+    getQuizes(state) {
+      let quizesToShow = state.quizes;
+      return quizesToShow;
+    }
   }
 })
