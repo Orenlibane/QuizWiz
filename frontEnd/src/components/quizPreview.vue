05594e0ef6b52f44d8a8b4855de76ed14c024fd9
Michael Ozery <michaelozery@gmail.com>
2019-07-14 17:36:38 +0300

    went through code till quizPreview

--- frontEnd/src/components/quizPreview.vue
+++ frontEnd/src/components/quizPreview.vue
@@ -8,23 +8,28 @@
       </div>
       <div class="flex both-align-center">
         <button @click="deleteQuiz(quiz._id)">🗑️</button>
-        <router-link :to="editUrl">
-          <button>Edit</button>
-        </router-link>
-        <router-link :to="enterGameUrl">
-          <button>Enter</button>
-        </router-link>
+        <button>
+          <router-link :to="editUrl">Edit</router-link>
+        </button>
+
+        <button>
+          <router-link :to="enterGameUrl">Enter</router-link>
+        </button>
       </div>
     </div>
     <div>
       <div class="flex column center space-between card-info">
-        <!-- <p>{{quiz.tags}}</p> -->
         <div class="tags-prev flex space-between">
           <p>{{quiz.tags[0]}}</p>
           <p>{{quiz.tags[1]}}</p>
         </div>
         <h3>{{quiz.title}}</h3>
-        <p>creator Name:</p>
+        <p>creator Name: {{quiz.creatorName}}</p>
+        <!-- We need to render the timer only if its a live-game room
+            Idea: when we will have live-game objects, they will have a countdown till game begins.
+            we'll check if th current quiz object has the countdownToGame attribute, and if it does,
+            we'll render the countdown.
+        -->
         <h3 class="right">30:00</h3>
       </div>
     </div>
@@ -38,8 +43,13 @@ import quizService from "../service/quizService.js";
 import _quizPreview from "../styles/components/_quiz-prev.scss";
 
 export default {
-  props: ["quiz"],
+  props: {
+    quiz: {
+      type: Object
+    }
+  },
   methods: {
+    // THOSE OPTIONS ARE ONLY FOR TESTING!!! The user won't really be able to delete games.
     deleteQuiz(quizId) {
       this.$store.dispatch({ type: "deleteQuiz", quizId }).then(() => {
         quizService.deleteQuiz(quizId);
@@ -56,5 +66,3 @@ export default {
   }
 };
 </script>
-<style>
-</style>
