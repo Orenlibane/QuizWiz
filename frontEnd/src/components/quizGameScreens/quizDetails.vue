b9e5b372b0bc06d10a271f7941ec2f3c015428ce
skendis <sergei.kendis@gmail.com>
2019-07-15 10:03:21 +0300

    quiz-details-scss

--- frontEnd/src/components/quizGameScreens/quizDetails.vue
+++ frontEnd/src/components/quizGameScreens/quizDetails.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="flex both-align-center column">
-    <div class="quiz-details">
+  <section class="quiz-details">
+    <!-- <div class="quiz-details">
       <router-link to="/">
         <button class="btn-quiz">
           <i class="fas fa-backward"></i>
@@ -26,12 +26,39 @@
           <i class="fas fa-thumbs-up"></i>
         </div>
       </div>
+    </div>-->
+    <router-link to="/">
+      <button class="small-btn">
+        <i class="fas fa-arrow-left"></i>
+      </button>
+    </router-link>
+    <div class="flex justify-center align-center column">
+      <h2>{{info.quiz.name}}</h2>
+      <p>by {{info.quiz.creatorName}}</p>
+    </div>
+    <div class="quiz-details-img">
+      <img src="@/assets/placeholder_600x400_2.jpg" alt />
+    </div>
+    <div class="flex column justify-center align-center">
+      <button class="mid-btn" @click="startSinglePlayer">Start</button>
+      <button class="mid-btn">Multiplayer</button>
+    </div>
+    <div class="details-footer layout-container flex justify-cener align-center space-between">
+      <div class="tags-show flex">
+        Tags:
+        <div v-for="(tag, idx) in info.quiz.tags" :key="idx">{{tag}}-</div>
+      </div>
+      <div class="like-box-container flex">
+        <button class="quiz-details-like-btn">
+          <i class="fas fa-thumbs-up"></i>
+        </button>
+        <span class="quiz-details-like-count">{{info.quiz.likesCount}}</span>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import global from "@/styles/global.scss";
 import quizService from "@/service/quizService.js";
 import utilService from "@/service/utilService.js";
 
@@ -55,9 +82,5 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-size: 3rem;
-  margin: 0;
-}
 </style>
 
