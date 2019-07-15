dd800c27765a35c00655102822f87cf478e76162
skendis <sergei.kendis@gmail.com>
2019-07-15 09:16:20 +0300

    card dev opts

--- frontEnd/src/components/quizPreview.vue
+++ frontEnd/src/components/quizPreview.vue
@@ -1,41 +1,5 @@
 <template>
-  <!-- <div class="card flex space-around">
-    <div class="img-container-card">
-      <img class="quiz-img-preview" src="@/assets/placeholder_600x400_2.jpg" alt />
-      <div class="likes-count">
-        <i class="fas fa-thumbs-up"></i>
-        {{quiz.likesCount}}
-      </div>
-      <div class="flex both-align-center">
-        <button @click="deleteQuiz(quiz._id)">🗑️</button>
-        <button>
-          <router-link :to="editUrl">Edit</router-link>
-        </button>
-
-        <button>
-          <router-link :to="enterGameUrl">Enter</router-link>
-        </button>
-      </div>
-    </div>
-    <div>
-      <div class="flex column center space-between card-info">
-        <div class="tags-prev flex space-between">
-          <p>{{quiz.tags[0]}}</p>
-          <p>{{quiz.tags[1]}}</p>
-        </div>
-        <h3>{{quiz.title}}</h3>
-  <p>creator Name: {{quiz.creatorName}}</p>-->
-  <!-- We need to render the timer only if its a live-game room
-            Idea: when we will have live-game objects, they will have a countdown till game begins.
-            we'll check if th current quiz object has the countdownToGame attribute, and if it does,
-            we'll render the countdown.
-  -->
-  <!-- <h3 class="right">30:00</h3>
-      </div>
-    </div>
-  </div>-->
   <section class="quiz-preview-card flex">
-
     <!-- IMG RIGHT SIDE WITH BTNS -->
     <div class="quiz-preview-img-container">
       <img class="quiz-img-preview" src="@/assets/placeholder_600x400_2.jpg" alt />
@@ -53,6 +17,16 @@
       <p>by {{quiz.creatorName}}</p>
       <div class="quiz-preview-timer">
         <span>00:59</span>
+      </div>
+      <div class="flex both-align-center">
+        <button @click="deleteQuiz(quiz._id)">🗑️</button>
+        <button>
+          <router-link :to="editUrl">Edit</router-link>
+        </button>
+
+        <button>
+          <router-link :to="enterGameUrl">Enter</router-link>
+        </button>
       </div>
     </div>
   </section>
