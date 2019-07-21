7668b55118492e05651a49d1b8c4009ccb584ab9
skendis <sergei.kendis@gmail.com>
2019-07-21 16:16:09 +0300

    header

--- frontEnd/src/components/appHeader.vue
+++ frontEnd/src/components/appHeader.vue
@@ -1,16 +1,8 @@
 <template>
-  <header class="app-header flex layout-container space-between both-align-center">
+  <header class="app-header flex layout-container justify-center align-center">
     <router-link to="/">
       <h1>QwizWizz</h1>
     </router-link>
-    <!-- <div class="header-btn-container flex both-align-center">
-      <button class="header-btn">
-        <i class="fas fa-search"></i>
-      </button>
-      <button class="header-btn">
-        <i class="fas fa-filter"></i>
-      </button>
-    </div>-->
   </header>
 </template>
 
